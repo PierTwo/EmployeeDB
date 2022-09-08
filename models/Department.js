264929cedcfeb1fbed621cc55d16891c1b6043b0
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-08 10:36:16 -0500

    Changed wording of mySQL query

--- models/Department.js
+++ models/Department.js
@@ -28,7 +28,7 @@ class Department extends BaseEntity {
   }
 
   utilizedBudgets() {
-    const query = `SELECT d.id as department_id, d.department_name, SUM(r.salary) AS utilized_budget, COUNT(e.id) AS number_of_employees 
+    const query = `SELECT d.id, d.department_name, SUM(r.salary) AS utilized_budget, COUNT(e.id) AS number_of_employees 
     FROM departments AS d
     INNER JOIN roles AS r
     ON d.id = r.department_id
@@ -40,7 +40,7 @@ class Department extends BaseEntity {
   }
 
   utilizedBudgetById(id) {
-    const query = `SELECT d.id as department_id, d.department_name, SUM(r.salary) AS utilized_budget, COUNT(e.id) AS number_of_employees 
+    const query = `SELECT d.id, d.department_name, SUM(r.salary) AS utilized_budget, COUNT(e.id) AS number_of_employees 
     FROM departments AS d
     INNER JOIN roles AS r
     ON d.id = r.department_id
