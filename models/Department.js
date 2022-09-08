bd45a21f902dd6d5dfe485917d66b603fb552bdd
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-08 08:09:45 -0500

    Added method to view utilized budget by id

--- models/Department.js
+++ models/Department.js
@@ -38,6 +38,18 @@ class Department extends BaseEntity {
 
     return this.dbConnection.query(query);
   }
+
+  utilizedBudgetById(id) {
+    const query = `SELECT d.id as department_id, d.department_name, SUM(r.salary) AS utilized_budget, COUNT(e.id) AS number_of_employees 
+    FROM departments AS d
+    INNER JOIN roles AS r
+    ON d.id = r.department_id
+    INNER JOIN employees AS e
+    ON r.id = e.role_id
+    WHERE d.id = ?`;
+
+    return this.dbConnection.query(query, id);
+  }
 }
 
 module.exports = Department;
