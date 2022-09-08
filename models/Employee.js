0ad22f0b7f125a4ba38d5acd9198cf337e85f2a6
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-08 01:51:09 -0500

    Added find by manager and find by department methods to Employee class

--- models/Employee.js
+++ models/Employee.js
@@ -35,6 +35,36 @@ class Employee extends BaseEntity {
     return super.findById(query, id);
   }
 
+  findByDepartment(id) {
+    const query = `SELECT e.id, e.first_name, e.last_name, r.title, d.department_name AS department, r.salary,
+    CONCAT(m.first_name, " ", m.last_name) AS manager
+    FROM employees AS e
+    JOIN roles AS r
+    ON e.role_id = r.id
+    JOIN departments AS d
+    ON r.department_id = d.id
+    LEFT JOIN employees AS m
+    ON e.manager_id = m.id
+    WHERE d.id = ?`;
+
+    return this.dbConnection.query(query, id);
+  }
+
+  findByManager(id) {
+    const query = `SELECT e.id, e.first_name, e.last_name, r.title, d.department_name AS department, r.salary,
+    CONCAT(m.first_name, " ", m.last_name) AS manager
+    FROM employees AS e
+    JOIN roles AS r
+    ON e.role_id = r.id
+    JOIN departments AS d
+    ON r.department_id = d.id
+    LEFT JOIN employees AS m
+    ON e.manager_id = m.id
+    WHERE e.manager_id = ?`;
+
+    return this.dbConnection.query(query, id);
+  }
+
   insert(obj) {
     return super.insert("employees", obj);
   }
