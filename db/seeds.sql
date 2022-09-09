3b66b51b9b8d40008250fcb533548b774051ab2d
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-09 01:42:01 -0500

    Added seeds for database

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO departments (department_name)
+VALUES ("Engineering"),
+("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Engineering Lead", 150000, 1),
+("Engineer", 120000, 1),
+("Sales Lead", 100000, 2);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Gandalf", "Mithrandir", 1, NULL),
+("Frodo", "Baggins", 2, 1),
+("Gimli", "Durin", 2, 1),
+("Pippin", "Took", 3, NULL);
